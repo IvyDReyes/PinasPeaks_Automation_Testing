5511611c412663334019153388ee251a9edf9da3
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-05-28 00:20:03 +1200

    Refactor code structure for improved readability and maintainability

--- playwright.config.js
+++ playwright.config.js
@@ -14,18 +14,33 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 10 * 3000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * Defaults to 5 seconds.
+     */
+    timeout: 6000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  // retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    // video: 'on',
+    // launchOptions: {
+    //   slowMo: 3000,
+      /* For debugging purposes, you can set the slowMo option to slow down Playwright operations by the specified amount of milliseconds. */
+      // slowMo: 1000,
+  
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
--- tests/profile.spec.js
+++ tests/profile.spec.js
@@ -1,19 +1,37 @@
-import { expect, test } from "@playwright/test";
+import { expect, test, chromium } from "@playwright/test";
 
-test("Verify if the Profile page of Ivey is loading successfully", async ({
-  page,
-}) => {
-  await test.step("Navigate to URL", async () => {
-    await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
-    await expect(page.getByRole('link', { name: 'PinasPeaks Logo' })).toBeVisible();
-    await page.screenshot({ path: 'screenshots/profile-page_navigate-to-url_loaded-visible_pass.png' });
-    await expect(page).toHaveTitle("PinasPeaks");
-    await page.waitForLoadState("networkidle");
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  await expect(
+    page.getByRole("link", { name: "PinasPeaks Logo" })
+  ).toBeVisible();
+  await page.screenshot({
+    path: "screenshots/profile-page_navigate-to-url_loaded-visible_pass.png",
   });
+  await expect(page).toHaveTitle("PinasPeaks");
+  await page.waitForLoadState("networkidle");
 });
 
+ test.afterAll('Close out', async () => {
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+
+        await page.close()
+    })
+
+// test("Verify if the Profile page of Ivey is loading successfully", async ({ page }) => {
+//   await test.step("Navigate to URL", async () => {
+//     await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+//     await expect(page.getByRole('link', { name: 'PinasPeaks Logo' })).toBeVisible();
+//     await page.screenshot({ path: 'screenshots/profile-page_navigate-to-url_loaded-visible_pass.png' });
+//     await expect(page).toHaveTitle("PinasPeaks");
+//     await page.waitForLoadState("networkidle");
+//   });
+// });
+
 test("Verify the profile picture and the username", async ({ page }) => {
-  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
 
   await test.step("Check if profile image is visible and clickable", async () => {
     const iveyImage = page.getByRole("img", { name: "Ivey" });
@@ -30,7 +48,7 @@ test("Verify the profile picture and the username", async ({ page }) => {
 test("Verify the Location, Last Climb and Favorite mountain", async ({
   page,
 }) => {
-  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
 
   await test.step("Check if the Location is visible and correct", async () => {
     await expect(page.getByText("Location:")).toBeVisible();
@@ -55,7 +73,7 @@ test("Verify the Location, Last Climb and Favorite mountain", async ({
 });
 
 test("Verify the number of Climbs and Peaks", async ({ page }) => {
-  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
 
   await test.step("Check if the number of Climbs is visible and correct", async () => {
     await expect(page.getByText("Climbs:")).toBeVisible();
@@ -69,7 +87,7 @@ test("Verify the number of Climbs and Peaks", async ({ page }) => {
 });
 
 test("Verify the Achievements and Connect", async ({ page }) => {
-  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
 
   await test.step("Check if the Achievements section is visible and correct", async () => {
     await expect(page.getByText("Achievements:")).toBeVisible();
@@ -81,67 +99,86 @@ test("Verify the Achievements and Connect", async ({ page }) => {
 });
 
 test("Verify if Order by dropdown is working", async ({ page }) => {
-  await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
 
-  await test.step('Check the Order by: dropdown', async () => {
-    const orderBy = page.getByText('Order by:');
+  await test.step("Check the Order by: dropdown", async () => {
+    const orderBy = page.getByText("Order by:");
     await expect(orderBy).toBeVisible();
-    await expect(orderBy).toHaveText('Order by:');
-    await expect(page.getByRole('combobox')).toBeVisible(); 
+    await expect(orderBy).toHaveText("Order by:");
+    await expect(page.getByRole("combobox")).toBeVisible();
+  });
+});
+
+test("Verify the Order by dropdown by Elevation is working", async ({
+  page,
+}) => {
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+
+  await test.step("Select the Order by Elevation (masl) in the dropdown", async () => {
+    await page.getByRole("combobox").selectOption({ value: "masl" });
+    const resultMasl = page
+      .locator("div")
+      .filter({ hasText: "Mt. Pulag2926 MASL Ifugao," })
+      .first();
+    await expect(resultMasl).toBeVisible();
+    await page.screenshot({
+      path: "screenshots/profile-page_order-by_masl-visible_pass.png",
+    });
+    await resultMasl.click();
+  });
+});
+
+test("Verify the Order by dropdown by Name is working", async ({ page }) => {
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+
+  await test.step("Select the Order by Name in the dropdown", async () => {
+    await page.getByRole("combobox").selectOption({ value: "name" });
+    const resultName = page
+      .locator("div")
+      .filter({ hasText: "Malvar's Peak419 MASL Rizal" })
+      .first();
+    await expect(resultName).toBeVisible();
+    await page.screenshot({
+      path: "screenshots/profile-page_order-by-name-visible_pass.png",
+    });
+    await resultName.click();
+  });
+});
+
+test("Verify the Order by dropdown by Date Climbed is working", async ({
+  page,
+}) => {
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+
+  await test.step("Select the Order by Date Climbed in the dropdown", async () => {
+    await page.getByRole("combobox").selectOption({ value: "date" });
+    const resultDate = page
+      .locator("div")
+      .filter({ hasText: "Mt. Kapayas783 MASL Cebu Day" })
+      .first();
+    await expect(resultDate).toBeVisible();
+    await page.screenshot({
+      path: "screenshots/profile-page_order-by-date_climbed-visible_pass.png",
+    });
+    await resultDate.click();
+  });
+});
+
+test("Verify the Search textbox", async ({ page }) => {
+  // await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
+
+  await test.step("Search with valid input", async () => {
+    const searchBox = page.getByRole("textbox", { name: "Search..." });
+    await expect(searchBox).toBeVisible();
+    await searchBox.fill("Pulag");
+    await page.screenshot({
+      path: "screenshots/profile-page_search-valid-input_visible_pass.png",
     });
   });
 
-  test("Verify the Order by dropdown by Elevation is working", async ({ page }) => {
-    await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
-
-    await test.step('Select the Order by Elevation (masl) in the dropdown', async () => {
-    await page.getByRole('combobox').selectOption({ value: 'masl' });
-    const resultMasl = page.locator('div').filter({ hasText: 'Mt. Pulag2926 MASL Ifugao,' }).first();
-    await expect(resultMasl).toBeVisible();
-    await page.screenshot({ path: 'screenshots/profile-page_order-by_masl-visible_pass.png' });
-    await resultMasl.click();
-     });
-});
-
-  test("Verify the Order by dropdown by Name is working", async ({ page }) => {
-    await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
-
-    await test.step('Select the Order by Name in the dropdown', async () => {
-    await page.getByRole('combobox').selectOption({ value: 'name' });
-    const resultName = page.locator('div').filter({ hasText: 'Malvar\'s Peak419 MASL Rizal' }).first();
-    await expect(resultName).toBeVisible();
-    await page.screenshot({ path: 'screenshots/profile-page_order-by-name-visible_pass.png' });
-    await resultName.click();
-     });
-});
-
-  test("Verify the Order by dropdown by Date Climbed is working", async ({ page }) => {
-    await page.goto("https://pinaspeaks.com/mountaineer/@Ivey");
-
-    await test.step('Select the Order by Date Climbed in the dropdown', async () => {
-    await page.getByRole('combobox').selectOption({ value: 'date' });
-    const resultDate = page.locator('div').filter({ hasText: 'Mt. Kapayas783 MASL Cebu Day' }).first();
-    await expect(resultDate).toBeVisible();
-    await page.screenshot({ path: 'screenshots/profile-page_order-by-date_climbed-visible_pass.png' });
-    await resultDate.click();
-     });
-});
-
-test('Verify the Search textbox', async ({ page }) => {
-  await page.goto('https://pinaspeaks.com/mountaineer/@Ivey');
-
-  await test.step('Search with valid input', async () => {
-    const searchBox = page.getByRole('textbox', { name: 'Search...' });
+  await test.step("Search with valid invalid input", async () => {
+    const searchBox = page.getByRole("textbox", { name: "Search..." });
     await expect(searchBox).toBeVisible();
-    await searchBox.fill('Pulag');
-    await page.screenshot({ path: 'screenshots/profile-page_search-valid-input_visible_pass.png' });
-    
-  });
-
-   await test.step('Search with valid invalid input', async () => {
-    const searchBox = page.getByRole('textbox', { name: 'Search...' });
-    await expect(searchBox).toBeVisible();
-    await searchBox.fill('Apo');
-    
+    await searchBox.fill("Apo");
   });
 });
